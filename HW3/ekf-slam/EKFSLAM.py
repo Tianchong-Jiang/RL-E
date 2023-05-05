eb0e425e137e887d10d383d8fedab790dcfdc9bd
Tianchong Jiang <tianchongjiang@gmail.com>
2023-05-05 15:04:48 -0500

    implemented, many bugs

--- HW3/ekf-slam/EKFSLAM.py
+++ HW3/ekf-slam/EKFSLAM.py
@@ -77,7 +77,24 @@ class EKFSLAM(object):
         """
 
         # TODO: Your code goes here
-        pass
+
+        # Predict the mean
+        self.mu[0] = self.mu[0] + u[0] * np.cos(self.mu[2])
+        self.mu[1] = self.mu[1] + u[0] * np.sin(self.mu[2])
+        self.mu[2] = self.angleWrap(self.mu[2] + u[1])
+
+        # Compute Jacobian of motion model
+        F = np.zeros((2, 3))
+        F[0, 0] = 1
+        F[0, 2] = -u[0] * np.sin(self.mu[2])
+        F[1, 1] = 1
+        F[1, 2] = u[0] * np.cos(self.mu[2])
+
+        # Predict the covariance
+        import pdb; pdb.set_trace()
+        self.Sigma[0:2, 0:2] = F @ self.Sigma[0:3, 0:3] @ F.T + self.R
+        self.Sigma[0:2, 2:] = F @ self.Sigma[0:3, 3:]
+        self.Sigma[2:, 0:2] = self.Sigma[0:3, 3:] @ F.T
 
     def update(self, z, id):
         """Perform the measurement update step to compute the posterior
@@ -91,7 +108,25 @@ class EKFSLAM(object):
             id : The ID of the observed landmark (int)
         """
         # TODO: Your code goes here
-        pass
+
+        # Compute Jacobian of measurement model
+        H = np.zeros((2, 3))
+        H[0, 0] = -np.cos(self.mu[2])
+        H[0, 1] = -np.sin(self.mu[2])
+        H[0, 2] = -z[0] * np.sin(self.mu[2]) + z[1] * np.cos(self.mu[2])
+        H[1, 0] = np.sin(self.mu[2])
+        H[1, 1] = -np.cos(self.mu[2])
+        H[1, 2] = z[0] * np.cos(self.mu[2]) + z[1] * np.sin(self.mu[2])
+
+        # Compute Kalman gain
+        K = self.Sigma[0:3, 0:3] @ H.T  @ np.linalg.inv(self.H @ self.Sigma[0:3, 0:3] @ H.T + self.Q)
+
+        # Update the mean
+        self.mu[0:3] = self.mu[0:3] + K @ (z - self.mu[0:3])
+
+        # Update the covariance
+        self.Sigma[0:3, 0:3] = (np.eye(3) - K @ H) @ self.Sigma[0:3, 0:3]
+
 
     def augmentState(self, z, id):
         """Augment the state vector to include the new landmark
@@ -104,7 +139,30 @@ class EKFSLAM(object):
         """
 
         # TODO: Your code goes here
-        pass
+
+        # Check if the landmark has already been added
+        if id in self.mapLUT.items():
+            return
+
+        # Add the landmark to LUT
+        self.mapLUT[id] = (self.mu.shape[0] - 1) * 0.5
+
+        # Compute the landmark position in the world frame
+        x = self.mu[0] + z[0] * np.cos(self.mu[2]) - z[1] * np.sin(self.mu[2])
+        y = self.mu[1] - z[0] * np.sin(self.mu[2]) - z[1] * np.cos(self.mu[2])
+
+        # Augment the mean
+        self.mu = np.append(self.mu, np.array((x, y)))
+
+        # Compute G
+        G = np.asarray([[1, 0, z[0] * np.sin(self.mu[2]) + z[1] * np.cos(self.mu[2])],
+                        [0, 1, z[0] * - np.cos(self.mu[2]) + z[1] * np.sin(self.mu[2])]])
+
+        # Augment the covariance
+        self.Sigma[0:3, 3:] = G @ self.Sigma[0:3, 3:]
+        self.Sigma[3:, 0:3] = self.Sigma[0:3, 3:] @ G.T
+        self.Sigma[3:, 3:] = G @ self.Sigma[3:, 3:] @ G.T + self.Q
+
 
     def angleWrap(self, theta):
         """Ensure that a given angle is in the interval (-pi, pi)."""
@@ -128,7 +186,6 @@ class EKFSLAM(object):
                   and the observed (x, y) position relative to the robot
         """
         # TODO: Your code goes here
-        pass
 
         # You may want to call the visualization function between filter steps where
         #       self.XGT[1:4, t] is the column of XGT containing the pose the current iteration
@@ -137,3 +194,11 @@ class EKFSLAM(object):
         #                   and the index in mu is the value
         #
         # self.renderer.render(self.mu, self.Sigma, self.XGT[1:4, t], Zt, self.mapLUT)
+
+        for i in range(U.shape[1]):
+            self.prediction(U[:, i])
+            for j in range(Z.shape[1]):
+                if Z[0, j] == i:
+                    self.update(Z[2:4, j], Z[1, j])
+                    self.augmentState(Z[2:4, j], Z[1, j])
+            self.renderer.render(self.mu, self.Sigma, self.XGT[1:4, i], Z[:, Z[0, :] == i], self.mapLUT)
